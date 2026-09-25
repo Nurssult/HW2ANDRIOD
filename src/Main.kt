2bc5fc14806ab2a62229481643e8d2f7114ce90e
Nurssult <nursultansat88@gmail.com>
2026-09-25 22:59:41 +0500

    HW2:Task7

--- src/Main.kt
+++ src/Main.kt
@@ -1,40 +1,12 @@
-open class Phone(var isScreenLightOn: Boolean = false){
-    open fun switchOn() {
-        isScreenLightOn = true
-    }
-
-    fun switchOff() {
-        isScreenLightOn = false
-    }
-
-    fun checkPhoneScreenLight() {
-        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
-        println("The phone screen's light is $phoneScreenLight.")
-    }
-}
-
-class FoldablePhone(var isFolded: Boolean = true): Phone() {
-    override fun switchOn() {
-        if (!isFolded) {
-            isScreenLightOn = true
-        }
-    }
-
-    fun fold() {
-        isFolded = true
-    }
-
-    fun unfold() {
-        isFolded = false
-    }
-}
-
 fun main() {
-    val newFoldablePhone = FoldablePhone()
+    val winningBid = Bid(5000, "Private Collector")
 
-    newFoldablePhone.switchOn()
-    newFoldablePhone.checkPhoneScreenLight()
-    newFoldablePhone.unfold()
-    newFoldablePhone.switchOn()
-    newFoldablePhone.checkPhoneScreenLight()
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
+}
+
+class Bid(val amount: Int, val bidder: String)
+
+fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+    return bid?.amount ?: minimumPrice
 }
