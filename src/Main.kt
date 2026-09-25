cf4b2eb6311d7553318ee35a9f899c357a663043
Nurssult <nursultansat88@gmail.com>
2026-09-25 22:54:10 +0500

    HW2:Task 5

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,30 @@
-class Song(
-    val title: String,
-    val artist: String,
-    val yearPublished: Int,
-    val playCount: Int
-){
-    val isPopular: Boolean
-        get() = playCount >= 1000
-
-    fun printDescription() {
-        println("$title, performed by $artist, was released in $yearPublished.")
-    }
-}
 fun main() {
-    val Song = Song("Thriller", "Michael Jackson", 1982, 1_000_000)
-    Song.printDescription()
-    println(Song.isPopular)
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        println("Name: $name")
+        println("Age: $age")
+        if(hobby != null) {
+            print("Likes to $hobby. ")
+        }
+        if(referrer != null) {
+            print("Has a referrer named ${referrer.name}")
+            if(referrer.hobby != null) {
+                print(", who likes to ${referrer.hobby}.")
+            } else {
+                print(".")
+            }
+        } else {
+            print("Doesn't have a referrer.")
+        }
+        println()
+        println()
+    }
 }
